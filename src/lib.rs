7ac442bb4c851f7eb6de58d78c35e1f2f05952e1
Bryce Mecum <petridish@gmail.com>
2024-12-30 10:03:20 -0800

    WIP fiddling with VirtualProtectEx

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,24 @@
-use retour::static_detour;
-use std::error::Error;
+use once_cell::sync::Lazy;
+use retour::GenericDetour;
 use std::ffi::c_int;
-use std::mem;
 use std::os::raw::c_void;
+use std::ptr::null_mut;
+use widestring::U16String;
+use windows::core::w;
 use windows::core::PCSTR;
+use windows::core::PCWSTR;
 use windows::Win32::Foundation::BOOL;
 use windows::Win32::Foundation::HANDLE;
 use windows::Win32::Storage::FileSystem::{
     CreateFileA, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_WRITE, FILE_SHARE_WRITE, OPEN_EXISTING,
 };
 use windows::Win32::System::Console::STD_ERROR_HANDLE;
+use windows::Win32::System::Memory::VirtualProtect;
+use windows::Win32::System::Memory::VirtualProtectEx;
+use windows::Win32::System::Memory::PAGE_EXECUTE_READWRITE;
+use windows::Win32::System::Memory::PAGE_PROTECTION_FLAGS;
+use windows::Win32::UI::WindowsAndMessaging::MessageBoxW;
+use windows::Win32::UI::WindowsAndMessaging::MB_OK;
 
 use windows::Win32::System::Console::{AllocConsole, SetStdHandle, STD_OUTPUT_HANDLE};
 
@@ -19,9 +28,12 @@ use windows::Win32::System::SystemServices::{
 
 // Chorizite has this as
 // private static int RecvFromImpl(nint s, byte* buf, int len, int flags, byte* from, int fromlen) {
-static_detour! {
-  static RecvFromImplHook: unsafe extern "system" fn(*mut c_void, *mut u8, c_int, c_int, *mut u8, c_int) -> c_int;
-}
+// static_detour! {
+//   static RecvFromImplHook: unsafe extern "system" fn(*mut c_void, *mut u8, c_int, c_int, *mut u8, c_int) -> c_int;
+// }
+
+type FnRecvFromImplHook =
+    unsafe extern "system" fn(*mut c_void, *mut u8, c_int, c_int, *mut u8, c_int) -> c_int;
 
 unsafe fn allocate_console() -> windows::core::Result<()> {
     unsafe {
@@ -68,24 +80,7 @@ pub fn is_executable_address(address: *const ()) -> bool {
         .contains(region::Protection::EXECUTE)
 }
 
-unsafe fn main() -> Result<(), Box<dyn Error>> {
-    allocate_console()?;
-
-    let address: i32 = 0x007935AC;
-
-    if is_executable_address(address as *const ()) {
-        println!("hook target address 0x{address:x} IS executable");
-    } else {
-        println!("hook target address 0x{address:x} NOT executable");
-    }
-
-    RecvFromImplHook
-        .initialize(mem::transmute(address), my_recv_from_impl_hook)?
-        .enable()?;
-    Ok(())
-}
-
-fn my_recv_from_impl_hook(
+extern "system" fn my_recv_from_impl_hook(
     s: *mut c_void,
     buf: *mut u8,
     len: c_int,
@@ -98,40 +93,74 @@ fn my_recv_from_impl_hook(
     println!("  Buffer length: {}", len);
     println!("  Flags: {}", flags);
     println!("  From length: {}", fromlen);
-
-    // Call the original function
-    let result = unsafe { RecvFromImplHook.call(s, buf, len, flags, from, fromlen) };
-
-    // If the call was successful, print the received data
-    // if result > 0 {
-    //     let received_data = std::slice::from_raw_parts(buf, result as usize);
-    //     println!("Received data: {:?}", received_data);
-    // }
-
-    println!("RecvFromImpl returned: {}", result);
-
-    result
+    unsafe { HOOK_RECV.disable().unwrap() };
+    let ret_val = unsafe { HOOK_RECV.call(s, buf, len, flags, from, fromlen) };
+    unsafe { HOOK_RECV.enable().unwrap() };
+    ret_val
 }
+static HOOK_RECV: Lazy<GenericDetour<FnRecvFromImplHook>> = Lazy::new(|| {
+    unsafe { allocate_console().unwrap() };
+    let address: i32 = 0x007935AC;
+
+    let ori: FnRecvFromImplHook = unsafe { std::mem::transmute(address) };
+    return unsafe { GenericDetour::new(ori, my_recv_from_impl_hook).unwrap() };
+});
 
 #[no_mangle]
 unsafe extern "system" fn DllMain(_hinst: HANDLE, reason: u32, _reserved: *mut c_void) -> BOOL {
     match reason {
         DLL_PROCESS_ATTACH => {
-            println!("In DllMain, attaching because reason is DLL_PROCESS_ATTACH");
-
+            println!("attaching");
             unsafe {
-                match main() {
+                // let address: i32 = 0x007935AC;
+                let address: *mut c_void = 0x007935AC as *mut c_void;
+                let mut old_protect: u32 = 0;
+
+                // pub unsafe fn VirtualProtectEx<P0>(
+                //     hprocess: P0,
+                //     lpaddress: *const c_void,
+                //     dwsize: usize,
+                //     flnewprotect: PAGE_PROTECTION_FLAGS,
+                //     lpfloldprotect: *mut PAGE_PROTECTION_FLAGS,
+                // ) -> Result<()>
+                // where
+                //     P0: Param<HANDLE>,
+
+                // Chorizite code:
+                // VirtualProtectEx(Process.GetCurrentProcess().Handle, address, (UIntPtr)4, 0x40, out int b);
+                // *(int*)address = newValue;
+                // VirtualProtectEx(Process.GetCurrentProcess().Handle, address, (UIntPtr)4, b, out b);
+
+                println!("Trying VirtualProtectEx");
+                let old_protect: *mut PAGE_PROTECTION_FLAGS = std::ptr::null_mut();
+                match VirtualProtectEx(
+                    _hinst,
+                    address,
+                    4 as usize,
+                    PAGE_EXECUTE_READWRITE,
+                    old_protect,
+                ) {
+                    Ok(_) => todo!(),
+                    Err(error) => {
+                        let lptext =
+                            U16String::from_str(format!("Attach failed: {error:?}").as_str());
+                        MessageBoxW(None, PCWSTR(lptext.as_ptr()), w!("Alembic"), MB_OK);
+                    }
+                }
+                println!("Done VirtualProtectEx");
+
+                match HOOK_RECV.enable() {
                     Ok(_) => {
-                        println!("main ran successfully")
+                        println!("Hook enable success");
                     }
                     Err(error) => {
-                        eprintln!("main ran with error {error:?}");
+                        println!("Hook enable error: {error:?}")
                     }
                 }
             }
         }
         DLL_PROCESS_DETACH => {
-            println!("In Dllmain, detaching because reason is DLL_PROCESS_DETACH");
+            println!("detaching");
         }
         DLL_THREAD_ATTACH => {}
         DLL_THREAD_DETACH => {}
