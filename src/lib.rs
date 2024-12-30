0783b6bb91039014ff0c5c84217f4b109a247a79
Bryce Mecum <petridish@gmail.com>
2024-12-29 22:00:31 -0800

    Working MessageBoxW detour
    This works at this point against a test application.

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,65 @@
-use std::io::Error;
-
-use widestring::U16CString;
-use windows::{
-    core::PCWSTR,
-    Win32::{Foundation::HWND, UI::WindowsAndMessaging::*},
+use retour::static_detour;
+use std::error::Error;
+use std::ffi::c_int;
+use std::os::raw::c_void;
+use std::{ffi::CString, iter, mem};
+use windows::core::{w, PCSTR, PCWSTR};
+use windows::Win32::Foundation::{BOOL, HANDLE, HWND};
+use windows::Win32::System::LibraryLoader::{GetModuleHandleW, GetProcAddress};
+use windows::Win32::System::SystemServices::{
+    DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH,
 };
 
-unsafe fn show_message_box(title: &str, message: &str) -> Result<(), Error> {
-    let lptext = U16CString::from_str(title).unwrap();
-    let lpcaption = U16CString::from_str(&message).unwrap();
-    let result = MessageBoxW(
-        HWND::default(),
-        PCWSTR(lptext.as_ptr()),
-        PCWSTR(lpcaption.as_ptr()),
-        MB_OK,
-    );
+static_detour! {
+  static MessageBoxWHook: unsafe extern "system" fn(HWND, PCWSTR, PCWSTR, u32) -> c_int;
+}
 
-    println!("got result of {result:?}");
+type FnMessageBoxW = unsafe extern "system" fn(HWND, PCWSTR, PCWSTR, u32) -> c_int;
 
+unsafe fn main() -> Result<(), Box<dyn Error>> {
+    let address = get_module_symbol_address("user32.dll", "MessageBoxW")
+        .expect("could not find 'MessageBoxW' address");
+    let target: FnMessageBoxW = mem::transmute(address);
+
+    MessageBoxWHook
+        .initialize(target, messageboxw_detour)?
+        .enable()?;
     Ok(())
 }
 
-#[ctor::ctor]
-fn ctor() {
-    println!("lib hello");
+fn messageboxw_detour(hwnd: HWND, text: PCWSTR, _caption: PCWSTR, msgbox_style: u32) -> c_int {
+    let replaced_caption = w!("Detoured!");
+    unsafe { MessageBoxWHook.call(hwnd, text, replaced_caption, msgbox_style) }
+}
 
+fn get_module_symbol_address(module: &str, symbol: &str) -> Option<usize> {
+    let module = module
+        .encode_utf16()
+        .chain(iter::once(0))
+        .collect::<Vec<u16>>();
+    let symbol = CString::new(symbol).unwrap();
     unsafe {
-        show_message_box("Hello", "World");
+        let handle = GetModuleHandleW(PCWSTR(module.as_ptr() as _)).unwrap();
+        match GetProcAddress(handle, PCSTR(symbol.as_ptr() as _)) {
+            Some(func) => Some(func as usize),
+            None => None,
+        }
     }
+}
+
+#[no_mangle]
+unsafe extern "system" fn DllMain(_hinst: HANDLE, reason: u32, _reserved: *mut c_void) -> BOOL {
+    match reason {
+        DLL_PROCESS_ATTACH => {
+            println!("attaching");
+            unsafe { main().unwrap() }
+        }
+        DLL_PROCESS_DETACH => {
+            println!("detaching");
+        }
+        DLL_THREAD_ATTACH => {}
+        DLL_THREAD_DETACH => {}
+        _ => {}
+    };
+    return BOOL::from(true);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
-use dll_syringe::{process::OwnedProcess, Syringe};
+use std::sync::mpsc::channel;
+
+use dll_syringe::{
+    process::{OwnedProcess, Process},
+    Syringe,
+};
 
 fn main() {
-    println!("Hello from main!");
+    let (tx, rx) = channel();
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
 
-    let target_process = OwnedProcess::find_first_by_name("Notepad").unwrap();
+    let target_process;
+
+    match OwnedProcess::find_first_by_name("WindowsProject1") {
+        Some(value) => {
+            target_process = value;
+        }
+        None => {
+            println!("Couldn't find process by name with error: Exiting.");
+            return;
+        }
+    }
+
+    // debugging
+    if OwnedProcess::is_x86(&target_process).unwrap() {
+        println!("target is 32-bit");
+    } else {
+        println!("target is not 32-bit");
+    }
+
     let syringe = Syringe::for_process(target_process);
-    let injected_payload = syringe.inject("target/debug/alembic.dll").unwrap();
+    let injected_payload = syringe
+        .inject("target\\i686-pc-windows-msvc\\debug\\alembic.dll")
+        .unwrap();
+
+    println!("DLL injected successfully!");
+
+    rx.recv().expect("Could not receive from channel.");
+    println!("Got it! Ejecting and exiting...");
+
     syringe.eject(injected_payload).unwrap();
 }
