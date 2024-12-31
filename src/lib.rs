3685a88d9f3d231014eef338e35837bfbd8a9604
Bryce Mecum <petridish@gmail.com>
2024-12-30 20:44:10 -0800

    Make launcher launch client
    Still doesn't work though

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,12 @@ use widestring::U16String;
 use windows::core::w;
 use windows::core::PCSTR;
 use windows::core::PCWSTR;
+use windows::Win32::Foundation::CloseHandle;
 use windows::Win32::Foundation::BOOL;
 use windows::Win32::Foundation::HANDLE;
+use windows::Win32::Security::GetTokenInformation;
+use windows::Win32::Security::TokenPrivileges;
+use windows::Win32::Security::TOKEN_QUERY;
 use windows::Win32::Storage::FileSystem::{
     CreateFileA, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_WRITE, FILE_SHARE_WRITE, OPEN_EXISTING,
 };
@@ -17,6 +21,12 @@ use windows::Win32::System::Memory::VirtualProtect;
 use windows::Win32::System::Memory::VirtualProtectEx;
 use windows::Win32::System::Memory::PAGE_EXECUTE_READWRITE;
 use windows::Win32::System::Memory::PAGE_PROTECTION_FLAGS;
+use windows::Win32::System::ProcessStatus::K32GetProcessImageFileNameA;
+use windows::Win32::System::Threading::GetCurrentProcess;
+use windows::Win32::System::Threading::GetCurrentProcessId;
+use windows::Win32::System::Threading::GetProcessId;
+use windows::Win32::System::Threading::OpenProcessToken;
+use windows::Win32::System::Threading::PROCESS_VM_OPERATION;
 use windows::Win32::UI::WindowsAndMessaging::MessageBoxW;
 use windows::Win32::UI::WindowsAndMessaging::MB_OK;
 
@@ -98,8 +108,8 @@ extern "system" fn my_recv_from_impl_hook(
     unsafe { HOOK_RECV.enable().unwrap() };
     ret_val
 }
+
 static HOOK_RECV: Lazy<GenericDetour<FnRecvFromImplHook>> = Lazy::new(|| {
-    unsafe { allocate_console().unwrap() };
     let address: i32 = 0x007935AC;
 
     let ori: FnRecvFromImplHook = unsafe { std::mem::transmute(address) };
@@ -108,14 +118,26 @@ static HOOK_RECV: Lazy<GenericDetour<FnRecvFromImplHook>> = Lazy::new(|| {
 
 #[no_mangle]
 unsafe extern "system" fn DllMain(_hinst: HANDLE, reason: u32, _reserved: *mut c_void) -> BOOL {
+    unsafe { allocate_console().unwrap() };
+
     match reason {
         DLL_PROCESS_ATTACH => {
             println!("attaching");
             unsafe {
                 // let address: i32 = 0x007935AC;
-                let address: *mut c_void = 0x007935AC as *mut c_void;
+                let address = 0x00793000;
                 let mut old_protect: u32 = 0;
 
+                // C++
+                // BOOL VirtualProtectEx(
+                //     [in]  HANDLE hProcess,
+                //     [in]  LPVOID lpAddress,
+                //     [in]  SIZE_T dwSize,
+                //     [in]  DWORD  flNewProtect,
+                //     [out] PDWORD lpflOldProtect
+                //   );
+                //
+                // Rust
                 // pub unsafe fn VirtualProtectEx<P0>(
                 //     hprocess: P0,
                 //     lpaddress: *const c_void,
@@ -126,37 +148,135 @@ unsafe extern "system" fn DllMain(_hinst: HANDLE, reason: u32, _reserved: *mut c
                 // where
                 //     P0: Param<HANDLE>,
 
-                // Chorizite code:
+                // Chorizite code example:
                 // VirtualProtectEx(Process.GetCurrentProcess().Handle, address, (UIntPtr)4, 0x40, out int b);
                 // *(int*)address = newValue;
                 // VirtualProtectEx(Process.GetCurrentProcess().Handle, address, (UIntPtr)4, b, out b);
 
+                // windbg output
+                // 0:022> !address 0x007935AC
+
+                // Mapping file section regions...
+                // Mapping module regions...
+                // Mapping PEB regions...
+                // Mapping TEB and stack regions...
+                // Mapping heap regions...
+                // Mapping page heap regions...
+                // Mapping other regions...
+                // Mapping stack trace database regions...
+                // Mapping activation context regions...
+
+                // Usage:                  Image
+                // Base Address:           00793000
+                // End Address:            0080b000
+                // Region Size:            00078000 ( 480.000 kB)
+                // State:                  00001000          MEM_COMMIT
+                // Protect:                00000002          PAGE_READONLY
+                // Type:                   01000000          MEM_IMAGE
+                // Allocation Base:        00400000
+                // Allocation Protect:     00000080          PAGE_EXECUTE_WRITECOPY
+                // Image Path:             C:\Games\AC\acclient.exe
+                // Module Name:            acclient
+                // Loaded Image Name:      C:\Games\AC\acclient.exe
+                // Mapped Image Name:
+                // More info:              lmv m acclient
+                // More info:              !lmi acclient
+                // More info:              ln 0x7935ac
+                // More info:              !dh 0x400000
+
+                // Content source: 1 (target), length: 77a54
+
+                let process_handle = GetCurrentProcess();
+                let process_id = GetProcessId(process_handle);
+
+                // Get process name (path)
+                let mut buffer = [0u8; 260]; // MAX_PATH
+                K32GetProcessImageFileNameA(process_handle, &mut buffer);
+                println!("Process ID: {}", process_id);
+                println!(
+                    "Process Path: {}",
+                    String::from_utf8_lossy(&buffer).trim_matches(char::from(0))
+                );
+
+                let mut privileges_length = 0;
+                let mut token_handle = HANDLE::default();
+
+                if OpenProcessToken(process_handle, TOKEN_QUERY, &mut token_handle).is_ok() {
+                    // First call to get required buffer size
+                    GetTokenInformation(
+                        token_handle,
+                        TokenPrivileges,
+                        Some(std::ptr::null_mut()),
+                        0,
+                        &mut privileges_length,
+                    );
+
+                    let mut buffer = vec![0u8; privileges_length as usize];
+
+                    // Second call to actually get privileges
+                    if GetTokenInformation(
+                        token_handle,
+                        TokenPrivileges,
+                        Some(buffer.as_mut_ptr() as *mut _),
+                        privileges_length,
+                        &mut privileges_length,
+                    )
+                    .is_ok()
+                    {
+                        // Check if PROCESS_VM_OPERATION is present
+                        // let has_vm_operation =
+                        //     (process_handle & PROCESS_VM_OPERATION.0) == PROCESS_VM_OPERATION.0;
+                        // println!("Has PROCESS_VM_OPERATION: {}", has_vm_operation);
+                        println!("We appear to have {process_handle:?}...")
+                    }
+
+                    CloseHandle(token_handle);
+                }
+
+                if process_handle == _hinst {
+                    println!("process is _hinst, FYI");
+                } else {
+                    println!("process is not _hinst, FYI");
+                }
+
+                if is_executable_address(address as *const ()) {
+                    println!("before virtualprotectex, address is executable");
+                } else {
+                    println!("before virtualprotectex, address is not executable");
+                }
+
                 println!("Trying VirtualProtectEx");
+                // VirtualProtectEx(Process.GetCurrentProcess().Handle, address, (UIntPtr)4, 0x40, out int b);
                 let old_protect: *mut PAGE_PROTECTION_FLAGS = std::ptr::null_mut();
                 match VirtualProtectEx(
-                    _hinst,
-                    address,
-                    4 as usize,
+                    process_handle,
+                    address as *const c_void,
+                    0x00078000 as usize,
                     PAGE_EXECUTE_READWRITE,
                     old_protect,
                 ) {
                     Ok(_) => todo!(),
                     Err(error) => {
-                        let lptext =
-                            U16String::from_str(format!("Attach failed: {error:?}").as_str());
-                        MessageBoxW(None, PCWSTR(lptext.as_ptr()), w!("Alembic"), MB_OK);
+                        eprintln!("VirtualProcessEx call failed: {error:?}")
                     }
                 }
                 println!("Done VirtualProtectEx");
 
-                match HOOK_RECV.enable() {
-                    Ok(_) => {
-                        println!("Hook enable success");
-                    }
-                    Err(error) => {
-                        println!("Hook enable error: {error:?}")
-                    }
+                if is_executable_address(address as *const ()) {
+                    println!("after virtualprotectex, address is executable");
+                } else {
+                    println!("after virtualprotectex, address is still not executable");
                 }
+
+                // Temporarily comment out until I figure out VirtualProtectEx
+                // match HOOK_RECV.enable() {
+                //     Ok(_) => {
+                //         println!("Hook enable success");
+                //     }
+                //     Err(error) => {
+                //         println!("Hook enable error: {error:?}")
+                //     }
+                // }
             }
         }
         DLL_PROCESS_DETACH => {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::sync::mpsc::channel;
+use std::{ffi::OsString, os::windows::ffi::OsStrExt, sync::mpsc::channel};
 
-use dll_syringe::{
-    process::{OwnedProcess, Process},
-    Syringe,
+use dll_syringe::{process::OwnedProcess, Syringe};
+use windows::{
+    core::PWSTR,
+    Win32::{
+        Foundation::{CloseHandle, GetLastError},
+        System::Threading::{
+            CreateProcessW, ResumeThread, CREATE_SUSPENDED, PROCESS_INFORMATION, STARTUPINFOW,
+        },
+    },
 };
 
 fn main() {
@@ -10,26 +16,57 @@ fn main() {
     ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
         .expect("Error setting Ctrl-C handler");
 
-    let target_process;
+    // launch acclient
+    let mut process_info: PROCESS_INFORMATION = unsafe { std::mem::zeroed() };
+    let mut startup_info: STARTUPINFOW = unsafe { std::mem::zeroed() };
+    startup_info.cb = std::mem::size_of::<STARTUPINFOW>() as u32;
 
-    match OwnedProcess::find_first_by_name("acclient") {
-        Some(value) => {
-            target_process = value;
-        }
-        None => {
-            println!("Couldn't find process by name with error: Exiting.");
-            return;
+    unsafe {
+        let cmd_line: Vec<u16> = OsString::from(
+            "C:\\Games\\AC\\acclient.exe -h play.coldeve.ac -p 9000 -a treestats -v treestats",
+        )
+        .encode_wide()
+        .chain(std::iter::once(0))
+        .collect();
+
+        let current_dir: Vec<u16> = OsString::from("C:\\Games\\AC")
+            .encode_wide()
+            .chain(std::iter::once(0))
+            .collect();
+
+        let result = CreateProcessW(
+            None,
+            PWSTR(cmd_line.as_ptr() as *mut _),
+            None,
+            None,
+            false,
+            CREATE_SUSPENDED,
+            None,
+            PWSTR(current_dir.as_ptr() as *mut _),
+            &startup_info,
+            &mut process_info,
+        );
+
+        match result {
+            Ok(_) => {
+                println!("Process created with ID: {}", process_info.dwProcessId);
+                let resume_result = ResumeThread(process_info.hThread);
+                if resume_result == u32::MAX {
+                    println!("Failed to resume thread. Last error: {:?}", GetLastError());
+                }
+                let _ = CloseHandle(process_info.hThread);
+                let _ = CloseHandle(process_info.hProcess);
+            }
+            Err(error) => {
+                eprintln!("CreateProcessW failure: {error:?}");
+            }
         }
     }
 
-    // debugging
-    if OwnedProcess::is_x86(&target_process).unwrap() {
-        println!("target is 32-bit");
-    } else {
-        println!("target is not 32-bit");
-    }
+    println!("Process is launched. Starting injection process...");
 
-    let syringe = Syringe::for_process(target_process);
+    let target = OwnedProcess::from_pid(process_info.dwProcessId).unwrap();
+    let syringe = Syringe::for_process(target);
 
     println!("About to inject");
 
