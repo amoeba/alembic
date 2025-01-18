607d333fb8ad7870b41c257459fee35e057e89b9
Bryce Mecum <petridish@gmail.com>
2025-01-17 21:12:51 -0800

    Get hardcoded accounts working

--- crates/desktop/src/backend.rs
+++ crates/desktop/src/backend.rs
@@ -10,11 +10,16 @@ pub struct PacketInfo {
     pub timestamp: u64,
     pub data: Vec<u8>,
 }
+pub struct Account {
+    pub name: String,
+}
 
 pub struct Backend {
     pub logs: Vec<LogEntry>,
     pub packets_incoming: Vec<PacketInfo>,
     pub packets_outgoing: Vec<PacketInfo>,
+    pub accounts: Vec<Account>,
+    pub selected_account: Option<usize>,
 }
 
 impl Backend {
@@ -23,6 +28,18 @@ impl Backend {
             logs: vec![],
             packets_incoming: vec![],
             packets_outgoing: vec![],
+            accounts: vec![
+                Account {
+                    name: "Frostfell - F".to_string(),
+                },
+                Account {
+                    name: "Leafcull - L".to_string(),
+                },
+                Account {
+                    name: "WintersEbb - W".to_string(),
+                },
+            ],
+            selected_account: None,
         }
     }
 }
--- crates/desktop/src/widgets/main_tab.rs
+++ crates/desktop/src/widgets/main_tab.rs
@@ -1,7 +1,50 @@
+use std::sync::{Arc, Mutex};
+
 use eframe::egui::{self, Response, Ui, Widget};
 
-use crate::launch::try_launch;
+use crate::{backend::Backend, launch::try_launch};
 
+use super::components::centered_text;
+
+struct AccountPicker {}
+
+impl Widget for &mut AccountPicker {
+    fn ui(self, ui: &mut Ui) -> Response {
+        if let Some(backend) =
+            ui.data_mut(|data| data.get_persisted::<Arc<Mutex<Backend>>>(egui::Id::new("backend")))
+        {
+            let mut backend = backend.lock().unwrap();
+
+            // TODO: Not sure if we have to do this immutable borrow
+            let account_names: Vec<String> =
+                backend.accounts.iter().map(|a| a.name.clone()).collect();
+
+            let selected_text = backend
+                .selected_account
+                .and_then(|index| account_names.get(index).cloned())
+                .unwrap_or_else(|| "Choose an account".to_string());
+
+            ui.group(|ui| {
+                egui::ComboBox::from_id_salt("Account")
+                    .selected_text(selected_text)
+                    .show_ui(ui, |ui| {
+                        for (index, name) in account_names.iter().enumerate() {
+                            ui.selectable_value(
+                                &mut backend.selected_account,
+                                Some(index),
+                                name.clone(),
+                            );
+                        }
+                    })
+                    .response
+            })
+            .response
+        } else {
+            ui.group(|ui| centered_text(ui, "Failed to reach application backend."))
+                .response
+        }
+    }
+}
 pub struct MainTab {}
 
 impl Widget for &mut MainTab {
@@ -18,14 +61,7 @@ impl Widget for &mut MainTab {
                 println!("Launch completed.");
             }
 
-            // let mut selected = ComboOptions::First;
-            // egui::ComboBox::from_label("Select one!")
-            //     .selected_text(format!("{:?}", selected))
-            //     .show_ui(ui, |ui| {
-            //         ui.selectable_value(&mut selected, ComboOptions::First, "First");
-            //         ui.selectable_value(&mut selected, ComboOptions::Second, "Second");
-            //         ui.selectable_value(&mut selected, ComboOptions::Third, "Third");
-            //     });
+            ui.add(&mut AccountPicker {})
         })
         .response
     }
