2e7d5939b0d72054948874a5c76d953c5482fcc1
Bryce Mecum <petridish@gmail.com>
2025-02-09 11:45:54 -0800

    Fix debug build script

--- scripts/build_debug.bat
+++ scripts/build_debug.bat
@@ -1,4 +1,4 @@
 @echo on
 
-cargo build --target i686-pc-windows-msvc -p dll --debug
-cargo build --target i686-pc-windows-msvc -p desktop --debug
+cargo build --target i686-pc-windows-msvc -p dll
+cargo build --target i686-pc-windows-msvc -p desktop
