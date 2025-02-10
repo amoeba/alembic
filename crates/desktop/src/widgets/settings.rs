8b4cc1dfa46bdd18617c2f4270c1a889aca830a9
Bryce Mecum <petridish@gmail.com>
2025-02-09 19:50:03 -0800

    Finish Settings layout

--- crates/desktop/src/widgets/settings.rs
+++ crates/desktop/src/widgets/settings.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{self, Response, Ui, Widget};
+use eframe::egui::{self, Align, Layout, Response, Ui, Widget};
 
 use crate::application::AppPage;
 
@@ -15,18 +15,29 @@ impl Settings {
 impl Widget for &mut Settings {
     fn ui(self, ui: &mut Ui) -> Response {
         ui.vertical(|ui| {
-            ui.heading("Settings");
-            ui.add_space(16.0);
-            ui.add(&mut SettingsGameClientPathEdit {});
-            ui.add_space(16.0);
-            ui.add(&mut SettingsDLLPathEdit {});
-
-            if ui.button("Exit").clicked() {
-                ui.memory_mut(|mem| {
-                    mem.data
-                        .insert_persisted(egui::Id::new("app_page"), AppPage::Main)
+            egui::TopBottomPanel::bottom("settings_controls")
+                .resizable(false)
+                .show_separator_line(false)
+                .show_inside(ui, |ui| {
+                    ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+                        if ui.button("Done").clicked() {
+                            ui.memory_mut(|mem| {
+                                mem.data
+                                    .insert_persisted(egui::Id::new("app_page"), AppPage::Main)
+                            });
+                        }
+                    });
                 });
-            }
+
+            egui::CentralPanel::default().show_inside(ui, |ui| {
+                ui.with_layout(Layout::top_down(Align::LEFT), |ui| {
+                    ui.heading("Settings");
+                    ui.add_space(16.0);
+                    ui.add(&mut SettingsGameClientPathEdit {});
+                    ui.add_space(16.0);
+                    ui.add(&mut SettingsDLLPathEdit {});
+                });
+            });
         })
         .response
     }
